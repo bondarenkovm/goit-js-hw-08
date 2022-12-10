3d8411ff8c800686f2e1dd27c902f066afcf96ee
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2022-12-10 20:52:31 +0200

    Завдання 1+

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const cardsImg = createCardsImgMarkup(galleryItems);
+// console.log(galleryEl);
+function createCardsImgMarkup(img) {
+  return img
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+          <a class="gallery__link" href='${original}'>
+            <img
+             class="gallery__image"
+             src='${preview}'
+             data-source='${original}'
+             alt='${description}'/>
+          </a>
+      </div>`;
+    })
+    .join('');
+}
+galleryEl.insertAdjacentHTML('afterbegin', cardsImg);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
