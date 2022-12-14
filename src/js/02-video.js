ce6b42668642f07b41e7e1a165a8514891e47cb2
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2022-12-14 16:02:52 +0200

    2-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+const VIMEO_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+timeVimeo();
+
+function onPlay({ seconds, duration }) {
+  // console.log(data);
+  localStorage.setItem(VIMEO_KEY, seconds);
+  if (seconds === duration) {
+    localStorage.removeItem(VIMEO_KEY);
+  }
+}
+function timeVimeo() {
+  if (localStorage.getItem(VIMEO_KEY)) {
+    player.setCurrentTime(localStorage.getItem(VIMEO_KEY));
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
 
+let formData = {};
+const FORM_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+setLocallyStoredDataForm();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(FORM_KEY)));
+  //   console.log(formData);
+  form.reset();
+  localStorage.removeItem(FORM_KEY);
+}
+function onFormInput(evt) {
+  //   console.log(evt.target.name);
+  //   console.log(evt.target.value);
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+}
+function setLocallyStoredDataForm() {
+  const locStor = JSON.parse(localStorage.getItem(FORM_KEY));
+  if (locStor) {
+    // formData = locStor;
+    // form.elements.email.value = locStor.email;
+    // form.elements.message.value = locStor.message;
+
+    for (const key in locStor) {
+      //   console.log(key);
+      //   console.log(locStor[key]);
+      formData[key] = locStor[key];
+      form.elements[key].value = locStor[key];
+    }
+  }
+}
